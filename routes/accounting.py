723bf239764f1597ae16bcbda30b60eca448aa62
fjalir <christoph.rohmann@googlemail.com>
2017-05-16 17:23:29 +0200

    PaymentState branch

--- routes/accounting.py
+++ routes/accounting.py
@@ -8,7 +8,7 @@ from marshmallow import Schema, fields
 from odie import sqla
 from login import get_user, login_required
 from api_utils import deserialize, api_route, ClientError
-from db.documents import Deposit, Document
+from db.documents import Deposit, Document, PaymentState
 
 import config
 
@@ -43,9 +43,9 @@ def log_deposit_return(data):
         doc = Document.query.get(data['document_id'])
         if doc is None:
             raise ClientError('document not found')
-        doc.deposit_return_eligible = False
+        doc.deposit_return_state = PaymentState.DISBURSED
         # data privacy, yo
-        if not doc.early_document_reward_eligible:
+        if doc.early_document_state is not PaymentState.ELIGIBLE:
             doc.submitted_by = None
 
     dep = Deposit.query.get(data['id'])
@@ -62,12 +62,12 @@ def log_early_document_reward(data):
     doc = Document.query.get(data['id'])
     if doc is None:
         raise ClientError('document not found')
-    if not doc.early_document_reward_eligible:
+    if doc.early_document_state is not PaymentState.ELIGIBLE:
         raise ClientError('document not eligible for early document reward')
 
-    doc.early_document_reward_eligible = False
+    doc.early_document_state = PaymentState.DISBURSED
     # data privacy, yo
-    if not doc.deposit_return_eligible:
+    if doc.deposit_return_state is not PaymentState.ELIGIBLE:
         doc.submitted_by = None
 
     db.accounting.log_early_document_disburse(get_user(), data['cash_box'])
